49f0dacba4748a002d3aefb22a856d542a1f442a
ishanbhatt42 <60279316+ishanbhatt42@users.noreply.github.com>
2020-05-22 21:08:04 -0500

    finished making numeric variables into characters

--- data-raw/make_cces.R
+++ data-raw/make_cces.R
@@ -1,19 +1,24 @@
 # Script for cleaning cces data (Shiro 18). My intention here is to create two
 # basic paths available for analysis. First, there's the basic analysis of whether
 # different demographics have significantly different answers. Second, there's the 
-# analysis of whether anything changed pre/post election.
+# analysis of approval of different levels of government official (pres, senator, 
+# governor).
 
-# So, I've kept most of the demographic information and the variable for whether
-# the individual took the post-election survey. Currently working on deciding 
-# which survey questions to keep. I discuss my choices below the select function.
+# So, initially, I've kept most of the demographic information. Currently working 
+# on deciding which survey questions to keep. I discuss my choices below the 
+# select function.
 
 library(tidyverse)
 library(usethis)
 
 x <- readRDS("data-raw/cumulative_2006_2018.rds") %>%
   select(year,
+
+# Case_id is pretty useless but necessary to see repeated
+# samples, so I kept it.
+
+         case_id,
          state,
-         tookpost, 
          gender, 
 
 # I kept age instead of birth year because it records the 
@@ -28,30 +33,79 @@ x <- readRDS("data-raw/cumulative_2006_2018.rds") %>%
 # ideology based question.
 
          ideology = ideo5,
-         newsint,
-         economy_retro,
+         news = newsint,
+         econ = economy_retro,
          approval_pres,
-         intent_pres_08,
-         intent_pres_12,
-         intent_pres_16,
-         voted_pres_08,
-         voted_pres_12,
-         voted_pres_16
-         )
-
+         approval_rep,
+         approval_sen1,
+         approval_sen2,
+         approval_gov) %>%
+  
 # I've kept three sort of groups of variables. The first is ideology and
 # news interest - I'm curious to see if there's a relationship between 
 # self-reported news interest and self-reported political ideology.
-
+  
 # The second is a retrospective view of the economy (how has the economy 
 # been doing this past year?) with the approval of the current president. That's probably
 # correlated? 
+  
+# The third is the approval ratings for senate officials, representatives, and governors. 
+# It would be interesting to see if they correlate with approval of the president
+# in some manner.
 
-# The third is the intent to vote for a certain president versus the actual vote 
-# for a president. This obviously only applies to the years 08, 12, and 16. The analysis
-# between who you intended to vote for and who you voted for can only be done for 
-# those took the post-election survey. Two things could be done here: A) thinking about
-# whether the retrospective view of the economy led to voting against the 
-# incumbent B) see whether the discrepancy in intent and voted for was different for
-# any of the elections.
+# Below, I convert some of the numerical representations of the data into
+# consistent character variables.
+
+  mutate(approval_pres = case_when(approval_pres == 1 ~ "Strongly Approve",
+                                  approval_pres == 2 ~ "Approve / Somewhat Approve",
+                                  approval_pres == 3 ~ "Disapprove / Somewhat Disapprove",
+                                  approval_pres == 4 ~ "Strongly Disapprove",
+                                  approval_pres == 5 ~ "Never Heard / Not Sure",
+                                  approval_pres == 6 ~ "Neither Approve Nor Disapprove")) %>%
+  
+  mutate(approval_gov = case_when(approval_gov == 1 ~ "Strongly Approve",
+                                   approval_gov == 2 ~ "Approve / Somewhat Approve",
+                                   approval_gov == 3 ~ "Disapprove / Somewhat Disapprove",
+                                   approval_gov == 4 ~ "Strongly Disapprove",
+                                   approval_gov == 5 ~ "Never Heard / Not Sure",
+                                   approval_gov == 6 ~ "Neither Approve Nor Disapprove")) %>%
+  
+  mutate(econ = case_when(econ == 1 ~ "Gotten Much Better",
+                          econ == 2 ~ "Gotten Better / Somewhat Better",
+                          econ == 3 ~ "Stayed About The Same",
+                          econ == 4 ~ "Gotten Worse / Somewhat Worse",
+                          econ == 5 ~ "Gotten Much Worse",
+                          econ == 6 ~ "Not Sure")) %>%
+  
+  mutate(news = case_when(news == 1 ~ "Most Of The Time",
+                          news == 2 ~ "Some Of The Time",
+                          news == 3 ~ "Only Now And Then",
+                          news == 4 ~ "Hardly At All",
+                          news == 7 ~ "Don't Know")) %>%
+  
+  mutate(gender = case_when(gender == 1 ~ "Male",
+                            gender == 2 ~ "Female")) %>%
+  
+  mutate(educ = case_when(educ == 1 ~ "No HS",
+                          educ == 2 ~ "High School Graduate",
+                          educ == 3 ~ "Some College",
+                          educ == 4 ~ "2-Year",
+                          educ == 5 ~ "4-Year",
+                          educ == 6 ~ "Post-Grad")) %>%
+  
+  mutate(race = case_when(race == 1 ~ "White",
+                          race == 2 ~ "Black",
+                          race == 3 ~ "Hispanic",
+                          race == 4 ~ "Asian",
+                          race == 5 ~ "Native American",
+                          race == 6 ~ "Mixed",
+                          race == 7 ~ "Other",
+                          race == 8 ~ "Middle Eastern")) %>%
+  
+  mutate(marstat = case_when(marstat == 1 ~ "Married",
+                          marstat == 2 ~ "Separated",
+                          marstat == 3 ~ "Divorced",
+                          marstat == 4 ~ "Widowed",
+                          marstat == 5 ~ "Single / Never Married",
+                          marstat == 6 ~ "Domestic Partnership"))
 
