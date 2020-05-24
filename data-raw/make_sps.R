00f983b1c583972806dffa46e63de5d203441fdb
Debora Gonzalez <deboragonzalez@college.harvard.edu>
2020-05-23 22:02:03 -0400

    updated variable set (14 including post-treatment)

--- data-raw/make_sps.R
+++ data-raw/make_sps.R
@@ -34,8 +34,53 @@ x <- read_dta("data-raw/sps.dta") %>%
   # Althought the dataverse readme says there is a Spanish and English codebook,
   # only the Spanish version is available on the site. 
   
-  select(age, treatment, urbrur, sex, educ, p12d13, p12d13_t2, p12d14, p12d14_t2, 
-         p12d20, p12d20_t2,p12d21, p12d21_t2, p12d22, p12d22_t2) %>% 
+  mutate(health_exp_3m = p03d15 + p03d16 + p03d17 + p03d18 + p03d19 + p03d20 + p03d21 + p03d22,
+         
+         # this adds together different types of medical expenses into a single
+         # 3-months health expenditures variable.
+         
+         t2_health_exp_3m = p03d15_t2 + p03d16_t2 + p03d17_t2 + p03d18_t2 + p03d19_t2 + p03d20_t2 + 
+           p03d21_t2 + p03d22_t2) %>% 
+  
+  # For questions 12.13 & 12.14, it appears that the answers were originally
+  # coded: 1 = agree, 2 = disagree, 9 = don't know. The new variable has been
+  # recorded to 0, 1, and NA.
+
+  # p12d13 Do you agree that: The government should try to reduce differences
+  # between rich and poor?
+  
+  # p12d14 Do you agree that: We should privatize the electricity industry to
+  # make it more efficient?
+  
+  rename(ideology = p12d20,
+         t2_ideology = p12d20_t2,
+         
+         # To which of the following political positions do you identify
+         # yourself the most?
+         
+         ec_better = p12d21,
+         t2_ec_better = p12d21_t2,
+         
+         # Compared to five years ago, do you think Mexico is better or worse
+         # today economically?
+         
+         pol_better = p12d22,
+         t2_pol_better = p12d22_t2,
+         
+         # Compared to five years ago, do you think Mexico is better or worse
+         # today politically?
+         
+         health_exp_1m = p03d11,
+         t2_health_exp_1m = p03d11_t2) %>% 
+  
+  
+  # Note that for the "_better" questions the responses seem to have been recorded
+  # 1 = Better = 1, 2 = Worse = -1, 3 = Same = 0, 8 = Don’t answer = NA, 9 = Don’t
+  # know = NA. This is my interpretation, but I am still trying to figure out if
+  # this is the actual recoding the authors used.
+  
+  select(age, sex, educ, treatment, ideology, t2_ideology, ec_better, t2_ec_better, pol_better, t2_pol_better, 
+         health_exp_3m, t2_health_exp_3m, health_exp_1m, t2_health_exp_1m)
   
 
 # From the readme on dataverse: A codebook for the survey questionnaire appears
@@ -50,48 +95,6 @@ x <- read_dta("data-raw/sps.dta") %>%
 # the section and 'd' is the question itself. Using the english questionnaire, I
 # identified some variables I thought were interesting. These variables can
 # certainly change to fit the needs and interests of the class.
-  
-# For questions 12.13 & 12.14, it appears that the answers were originally
-# coded: 1 = agree, 2 = disagree, 9 = don't know. The new variable has been
-# recorded to 0, 1, and NA.
-
-
-  rename(gov_resp_diff_rich_poor = p12d13,
-         t2_gov_resp_diff_rich_poor = p12d13_t2,
-         
-         # Do you agree that: The government should try to reduce differences
-         # between rich and poor?
-         
-         priv_industry_efficiency = p12d14,
-         t2_priv_industry_efficiency = p12d14_t2,
-
-         # Do you agree that: We should privatize the electricity industry to
-         # make it more efficient?
-         
-         ideology = p12d20,
-         t2_ideology = p12d20_t2,
-         
-         # To which of the following political positions do you identify
-         # yourself the most?
-         
-         ec_better = p12d21,
-         t2_ec_better = p12d21_t2,
-         
-         # Compared to five years ago, do you think Mexico is better or worse
-         # today economically?
-         
-         pol_better = p12d22,
-         t2_pol_better = p12d22_t2
-         
-         # Compared to five years ago, do you think Mexico is better or worse
-         # today politically?
-         
-         )
-
-# Note that for the "_better" questions the responses seem to have been recorded
-# 1 = Better = 1, 2 = Worse = -1, 3 = Same = 0, 8 = Don’t answer = NA, 9 = Don’t
-# know = NA. This is my interpretation, but I am still trying to figure out if
-# this is the actual recoding the authors used.
 
 
 sps <- x
