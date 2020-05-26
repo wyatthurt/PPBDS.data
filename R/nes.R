50a028529403ceb78a9fedba18126a5698dabed7
Beau <beau_meche@college.harvard.edu>
2020-05-26 01:55:33 -0500

    updating variable documentation for nes.rda

--- R/nes.R
+++ R/nes.R
@@ -5,7 +5,7 @@
 #' for background and details. First study conducted in 1948, see appendices for variations in questions
 #' and other metrics over time. Current as of 05/24/2020. 
 #'
-#' @format A tibble with 39,220 observations and 6 variables:
+#' @format A tibble with 39,220 observations and 7 variables:
 #' \describe{
 #'   \item{gender}{character variable with values "Male" and "Female"}
 #'   \item{income}{ordered factor of bracketed national income percentile values}
@@ -13,6 +13,7 @@
 #'   \item{year}{numeric variable for year of study}
 #'   \item{race}{character variable for race / ethnicity respondent identification}
 #'   \item{educ}{ordered factor variable with a 7 tier delineation of educational achievement}
+#'   \item{state}{character variable for the 2-letter abbreviation of the state of the interview of a respondent}
 #' }
 #' @source \url{https://electionstudies.org/data-center/anes-time-series-cumulative-data-file/}
 #'
