95fc112f5be5ff1bed4c0866f5c410f1c51f7b3c
Andy Wang <azwang@college.harvard.edu>
2020-05-26 02:17:25 -0500

    re-order columns

--- data-raw/make_qscores.R
+++ data-raw/make_qscores.R
@@ -123,9 +123,11 @@ qscores <- raw %>%
 
   # https://registrar.fas.harvard.edu/faculty-staff/courses/enrollment/archived-course-enrollment-reports
 
+  select(course_name, department, course_number, department, enrollment, workload, overall, prof_name) %>%
   as_tibble()
 
 
 # saving data
 
 usethis::use_data(qscores, overwrite = TRUE)
+
