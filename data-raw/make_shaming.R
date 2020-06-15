aa0c513648af6637c9283e11bffcd8db9eb3a9d9
Beau Meche <beau_meche@college.harvard.edu>
2020-06-15 14:05:05 -0400

    cleaned temp shaming dataset

---
+++ data-raw/make_shaming.R
@@ -0,0 +1,38 @@
+library(tidyverse)
+library(naniar)
+
+
+data <- read.csv("data-raw/social.csv")
+
+x <- data %>% 
+  mutate(gender = as.character(case_when(
+    sex == "male" ~ "Male",
+    sex == "female" ~ "Female",
+    T ~ NA_character_
+  ))) %>% 
+  
+  mutate(birth_yr = as.integer(yearofbirth)) %>% 
+  
+  mutate(treatment = as.factor(messages)) %>% 
+  
+  # this doesn't change the # of 
+  # records... this data is suspiciously clean
+  
+  # drop_na() %>%
+  
+  mutate(vote_04 = primary2004,
+         vote_06 = primary2006) %>% 
+  
+  select(-sex, -yearofbirth, -messages,
+         - primary2004, -primary2006)
+
+
+stopifnot(naniar::pct_miss(x) == 0)
+
+shaming <- x
+
+
+usethis::use_data(shaming, overwrite = T)
+
+
+
