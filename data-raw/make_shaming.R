cd1946093f48d5a2fc1a8e65f603f83c77fdc8ea
David Kane <dave.kane@gmail.com>
2020-06-25 09:16:52 -0400

    post TW meeting

--- data-raw/make_shaming.R
+++ data-raw/make_shaming.R
@@ -1,9 +1,12 @@
 library(tidyverse)
 
-x <- read_csv("data-raw/social.csv", 
-              
+
+# Where did social.csv come from? Quantitative Social Science.
+
+x <- read_csv("data-raw/social.csv",
+
               # get rid of the annoying reading notes for best practice
-              
+
               col_types = cols(
                 sex = col_character(),
                 yearofbirth = col_double(),
@@ -12,21 +15,21 @@ x <- read_csv("data-raw/social.csv",
                 primary2006 = col_double(),
                 hhsize = col_double()
               )) %>%
-  
+
   # there is a str_X (str_to_upper()??) function for this, but I am more comfortable with
   # this approach to missing data when cleaning new data the first time around.
   # Otherwise this would be shorter work, also I discovered later that there is no (0) missing
-  # data to me had in this original shaming data file... so here we are. 
-  
+  # data to me had in this original shaming data file... so here we are.
+
   mutate(gender = as.character(case_when(
     sex == "male" ~ "Male",
     sex == "female" ~ "Female",
     T ~ NA_character_
   ))) %>%
-  
+
   # bear in mind the publication year of this study, I was initially
   # uncomfortable with birth years ~1900, but this isn't a 2020 study
-  
+
   # parse_integer doesn't seem to want to work here
 
   mutate(birth_yr = as.integer(yearofbirth)) %>%
@@ -49,7 +52,7 @@ stopifnot(x %>% drop_na() %>% nrow() == 305866)
 
 shaming <- x
 
-usethis::use_data(shaming, overwrite = T)
+usethis::use_data(shaming, overwrite = TRUE)
 
 
 
