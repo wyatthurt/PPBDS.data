a4bbc27bd376925e04ea9a52e7b22f2c10983376
David Kane <dave.kane@gmail.com>
2020-06-29 21:31:00 -0400

    clean ups

--- R/sps.R
+++ R/sps.R
@@ -2,16 +2,16 @@
 #' (2009)
 #'
 #' Data for a randomised assessment of the Mexican universal health insurance
-#' programme, both before and after a natural experiment which randomly assigned
-#' treatment within 74 matched pairs of health clusters —representing 118 569
+#' programme, both before and after a study which randomly assigned
+#' treatment within 74 matched pairs of health clusters --- representing 118,569
 #' households in seven Mexican states. See
 #' \href{https://gking.harvard.edu/files/gking/files/pubpolforpoor.pdf}{King et
-#' al. (2009)} for background and details. measured outcomes in a 2005 baseline
+#' al. (2009)} for background and details. Data includes variables from a 2005 baseline
 #' survey (August, 2005, to September, 2005) and follow-up survey 10 months
-#' later (July, 2006, to August, 2006) in 50 pairs (n=32 515)
+#' later (July, 2006, to August, 2006).
 #'
 #'
-#' @format A tibble with 16733 observations and 8 variables: \describe{
+#' @format A tibble with 16,733 observations and 8 variables: \describe{
 #'   \item{age}{numerical variable with raw age values of respondents}
 #'   \item{sex}{character variable with values "Male" and "Female"}
 #'   \item{educ}{character variable with values "Preschool", "Primary",
@@ -23,5 +23,5 @@
 #'   health-related expenses in the last month in pesos}}
 #' @source
 #' \url{https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/P6NC0M}
-#' 
+#'
 "sps"
