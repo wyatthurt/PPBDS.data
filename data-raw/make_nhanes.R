9041dadd600b72fa701b492a9cb232d5f93287f3
Miroslav Bergam <mlbergam@gmail.com>
2020-07-01 20:13:11 -0400

    Cleaned more vars

--- data-raw/make_nhanes.R
+++ data-raw/make_nhanes.R
@@ -1,12 +1,15 @@
 # work in progress of the National Health and Nutrition Examination Survey
 
+# QUESTION: Should factor variables like race and gender just be characters?
+# QUESTION: NA or 0 for things like no pregnancies?
+
 library(NHANES)
 
 nhanes <- NHANES %>%
 
-  # Selecting relevant variables
+  # Selecting 5 relevant variables
 
-  select(ID, Gender, SurveyYr, Age, Race1, Education, HHIncome, Weight,
+  select(Gender, SurveyYr, Age, Race1, Education, HHIncome, Weight,
          Height, BMI, Pulse, Diabetes, HealthGen, Depressed, nPregnancies,
          SleepHrsNight) %>%
 
@@ -14,6 +17,22 @@ nhanes <- NHANES %>%
 
   separate(SurveyYr, into = c("survey_year", NA),
            sep = "_") %>%
+
+  # Also making it into an integer
+
+  mutate(survey_year = as.integer(survey_year)) %>%
+
+  # Fixing variable names
+
+  mutate(gender = as.character(Gender),
+         age = as.character(Age),
+         race = as.character(Race1),
+         sleep_night_hrs = SleepHrsNight,
+         height = Height,
+         bmi = BMI,
+         weight = Weight,
+         pregnancies = nPregnancies) %>%
+
 
 
 
