33f16e37c4e64e558953286ad32f92027f9413aa
Miroslav Bergam <mlbergam@gmail.com>
2020-07-01 20:34:38 -0400

    Worked on more variables

--- data-raw/make_nhanes.R
+++ data-raw/make_nhanes.R
@@ -1,9 +1,11 @@
 # work in progress of the National Health and Nutrition Examination Survey
 
-# QUESTION: Should factor variables like race and gender just be characters?
-# QUESTION: NA or 0 for things like no pregnancies?
+# MB: Should factor variables like race and gender just be characters?
+# MB: What should I be turning into number scales vs characters
 
 library(NHANES)
+
+testhanes <- NHANES
 
 nhanes <- NHANES %>%
 
@@ -22,6 +24,42 @@ nhanes <- NHANES %>%
 
   mutate(survey_year = as.integer(survey_year)) %>%
 
+  # Making depressed a numbered variable
+
+  mutate(depressed = as.integer(case_when(
+    Depressed == "None" ~ 0,
+    Depressed == "Several" ~ 1,
+    Depressed == "Most" ~ 2
+  ))) %>%
+
+  # Making general health a numbered variable
+
+  mutate(general_health = as.integer(case_when(
+    HealthGen == "Poor" ~ 1,
+    HealthGen == "Fair" ~ 2,
+    HealthGen == "Good" ~ 3,
+    HealthGen == "Vgood" ~ 4,
+    HealthGen == "Excellent" ~ 5
+  ))) %>%
+
+  # Cleaning up education variable names
+  # MB: Can I play with the buckets like this?
+
+  mutate(education = as.character(case_when(
+    Education == "8th Grade" ~ "middle school",
+    Education == "9 - 11th Grade" ~ "middle school",
+    Education == "High School" ~ "high school",
+    Education == "Some College" ~ "some college",
+    Education == "College Grade" ~ "college"
+  ))) %>%
+
+  # Making diabetes an integer variable
+
+  mutate(diabetes = as.integer(case_when(
+    Diabetes == "Yes" ~ 1,
+    Diabetes == "No" ~ 0,
+  ))) %>%
+
   # Fixing variable names
 
   mutate(gender = as.character(Gender),
@@ -31,7 +69,12 @@ nhanes <- NHANES %>%
          height = Height,
          bmi = BMI,
          weight = Weight,
-         pregnancies = nPregnancies) %>%
+         pregnancies = nPregnancies,
+         pulse = Pulse) %>%
+
+
+
+
 
 
 
