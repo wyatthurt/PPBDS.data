459fd6cf4adcda2b7494d262c036e9ba14a60fe7
Miroslav Bergam <mlbergam@gmail.com>
2020-07-07 09:56:29 -0400

    Cleaned primary variables

--- R/make_kenya.R
+++ R/make_kenya.R
@@ -1,5 +1,19 @@
 library(tidyverse)
 
-
 # diff read function
-datalong <- readRDS('data-raw/kenyadata.Rds')
+x <- readRDS('data-raw/kenyadata.Rds')
+
+kenyadata <- x %>%
+
+  mutate(treatment = as.character(case_when(treatment_2d_10d == "Control" ~ "control",
+                                            treatment_2d_10d == "SMS" ~ "SMS",
+                                            treatment_2d_10d == "Canvass" ~ "canvass",
+                                            treatment_2d_10d == "Local" ~ "local",
+                                            treatment_2d_10d == "Local+Canvass" ~ "local + canvass",
+                                            treatment_2d_10d == "Local+SMS" ~ "local + SMS"))) %>%
+
+  mutate(poverty = pov,
+         pop_density = pd) %>%
+
+  select(treatment, poverty, distance, pop_density)
+
